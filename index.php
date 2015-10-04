c4907be4613779c0cfc6dee0a4aeb7de125bf728
Tamra Fenstermaker <fenstermaker505@gmail.com>
2015-10-04 07:14:25 -0600

    Checking for the validity of a date in php

--- index.php
+++ index.php
@@ -1,9 +1,8 @@
-<!DOCTYPE html>
-<html>
-	<head>
-		<title>I completed the prework</title>
-	</head>
-	<body>
-		<p>I completed the prework!</p>
-	</body>
-</html>
+<?php
+$month = 9;  //September (only has 30 days)
+$day = 31; //31st
+$year = 2018; //2018
+
+if (checkdate($month, $day, $year)) echo "Date is valid";
+else echo "Date is invalid";
+?>
