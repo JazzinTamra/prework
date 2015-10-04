02950830bd032fc6732dcf5db25941003f030122
Tamra Fenstermaker <fenstermaker505@gmail.com>
2015-10-04 08:01:38 -0600

    System call php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ exec(escapeshellcmd($cmd), $output, $status);
 if ($status) echo "Exec command failed";
 else{
 	echo "<pre>";
-	foreach($output as $line) echo htmlspecialchars("$line<br>");
+	foreach($output as $line) echo htmlspecialchars("$line/n");
 	echo "</pre>";
 }
 
