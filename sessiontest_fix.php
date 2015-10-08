b58a9b23190b7b4609cf104e72f3ed4f6b88c9e8
Tamra Fenstermaker <fenstermaker505@gmail.com>
2015-10-08 05:30:15 -0600

    Session test that is NOT susceptible to session fixation.

--- sessiontest_fix.php
+++ sessiontest_fix.php
@@ -1,6 +1,11 @@
 <?php //sessiontext.php p305
 session_start();
 
+if (!isset($_SESSION['initiated']))
+{
+	session_regenerate_id();
+	$_SESSION['initiated'] = 1;
+}
 if (!isset($_SESSION['count'])) $_SESSION['count'] = 0;
 else ++$_SESSION['count'];
 
