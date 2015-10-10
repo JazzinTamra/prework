7b33ba25af6be2563c21f749d99ceb1f1c1d89b2
Tamra Fenstermaker <fenstermaker505@gmail.com>
2015-10-10 06:32:23 -0600

    Login file

---
+++ login2.php
@@ -0,0 +1,12 @@
+<?php //login.php
+$hn = 'localhost';
+$db = 'publication';
+$un = 'username';
+$pw = 'password';
+?>
+/**
+ * Created by PhpStorm.
+ * User: Tamra
+ * Date: 10/10/2015
+ * Time: 6:28 AM
+ */
