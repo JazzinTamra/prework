367fa1b66ff975b7627f56301b9ac78d2a48885a
Tamra Fenstermaker <fenstermaker505@gmail.com>
2015-10-10 07:11:41 -0600

    Hello World in JavaScript trying new file format.

---
+++ Hello_world.js
@@ -0,0 +1,11 @@
+<html>
+	<head><title>Hello World</title></head>
+	<body>
+		<script type="text/javascript">
+		document.write("Hello World")
+</script>
+<noscript>
+		Your broser doesn't support or has disabled JavaScript
+</nonscript>
+</body>
+</html>
